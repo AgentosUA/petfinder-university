895b710a44ccab86b9385a51fc63bd68e60b0d3b
Agentos <agentoswork@gmail.com>
2018-12-02 21:30:08 +0200

    reformed structure

---
+++ backend/react-app/src/components/Product.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import BuyButton from './BuyButton';
+
+class Product extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true, product: {} }
+  }
+
+  async componentDidMount() {
+    let response = await fetch(`https://snipcart-strapi.herokuapp.com/product/${this.props.match.params.id}`)
+    let data = await response.json()
+    this.setState({
+      loading: false,
+      product: data
+    })
+  }
+
+  render() {
+    if (!this.state.loading) {
+      return (
+        <div className="product">
+          <div className="product__information">
+            <h2 className="Product-title">{this.state.product.name}</h2>
+            <img src={`https://snipcart-strapi.herokuapp.com/${this.state.product.image.url}`} />
+            <BuyButton {...this.state} />
+          </div>
+          <div className="product__description">
+            {this.state.product.description}
+          </div>
+        </div>
+      );
+    }
+
+    return (<h2>Waiting for API...</h2>);
+  }
+}
+
+export default Product;
---
+++ backend/react-app/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Header from '../components/Header.js';
+import './style.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <Header />
+        );
+    }
+}
+
+export default Home;
