e4225e73b0b07443f503a5c9e31e3fa575ac29aa
AgentosUA <agentoswork@gmail.com>
2019-03-04 09:18:26 +0200

    Rebase backend init

---
+++ backend/api/routes/products.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'Handling GET requests to /products'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    const product = {
+        name: req.body.name,
+        price: req.body.price
+    };
+    res.status(201).json({
+        message: 'Handling POST request to /products',
+        createdProduct: product
+    });
+}); 
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    if(id === 'special') {
+        res.status(200).json({
+            message: 'special id!'
+        });
+    } else {
+        res.status(200).json({
+            message: 'You passed an ID'
+        });
+    }
+});
+
+router.patch('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Product updated!'
+    });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Product deleted!'
+    });
+});
+
+module.exports = router;
---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        erros: {
+            message:  error.message
+        }
+    });
+});
+
+module.exports = app;
