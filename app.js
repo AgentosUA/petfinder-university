491e850902cb8a523fd837e89135a9e556e7dfe8
= <=>
2019-03-26 19:43:46 +0200

    trying to connect mongodb

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+// db
+const MongoClient = require('mongodb').MongoClient;
+const dbUrl = "mongodb://127.0.0.1:27017";
+// devParser
 const bodyParser = require('body-parser')
 
-const petsRoutes = require('./api/routes/pets');
-const orderRoutes = require('./api/routes/orders');
+// routes
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+// const petsRoutes = require('./api/routes/pets');
+// const orderRoutes = require('./api/routes/orders');
 
-app.use('/pets', petsRoutes);
-app.use('/orders', orderRoutes);
+// app.use(morgan('dev'));
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+
+// app.use('/pets', petsRoutes);
+// app.use('/orders', orderRoutes);
+
+
+// error handler
 
 app.use((req, res, next) => {
     const error = new Error('not found');
@@ -28,4 +37,9 @@ app.use((req, res, next) => {
     });
 });
 
+MongoClient.connect(dbUrl, function(err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+  }, {  useNewUrlParser: true} );
 module.exports = app;
