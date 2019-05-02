b5eaabfedf26e601ab90ea7e9fe3ad89bdefaa35
Agentos <agentoswork@gmail.com>
2019-05-02 13:59:48 +0300

    removed onClick func & added eventListener

--- client/src/js/menu.js
+++ client/src/js/menu.js
@@ -1,8 +1,8 @@
-function myFunction() {
+menu.addEventListener("click", () => {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        menu.className = "topnav";
     }
-}
+});
