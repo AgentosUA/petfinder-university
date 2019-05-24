37328e22ea3b28d551d0e782e122a5ebdd8a0cd4
Agentos <agentoswork@gmail.com>
2019-05-24 11:03:10 +0300

    fixed error handling

--- app.js
+++ app.js
@@ -33,15 +33,20 @@ app.use('/user', userRoutes);
 
 // error handler
 
-
-
 app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res) => {
     res.status(error.status || 500);
     res.json({
-        erros: {
+        error: {
             message:  error.message
         }
     });
+    res.redirect('https://www.google.com/');
 });
 
 module.exports = app;
