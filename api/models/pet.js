33702ed2150bd7f36cf4fa20f5582a2edd9b0f6e
= <=>
2019-09-30 13:31:12 +0300

    removed hardcoded id from pet model

--- api/models/pet.js
+++ api/models/pet.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const petSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
   name: { type: String, required: true },
   type: { type: String, required: true },
   gender: { type: String, required: true },
