0a70a03669177fc7be28f103b56f4e6b0dd74e45
= <=>
2019-11-27 23:36:29 +0200

    fixed _id type in model & controller

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,31 +1,32 @@
+const mongoose = require('mongoose');
 const User = require('../models/user.js');
 
 exports.login = (req, res, next) => {
-	let id = req.params.id;
-	User.findOne({ _id: id }).then(user => {
-		res.json({
-			userName: user.name
-		});
-	});
+  let id = req.params.id;
+  User.findOne({ _id: id }).then(user => {
+    res.json({
+      userName: user.name
+    });
+  });
 };
 
 exports.signup = (req, res, next) => {
-	const user = new User({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
-	});
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
 
-	user
-		.save()
-		.then(result => {
-			res.status(201).json({
-				message: 'User succsessfully added!'
-			});
-		})
-		.catch(err => {
-			res.status(409).json({
-				error: 'User already exists'
-			});
-		});
+  user
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'User succsessfully added!'
+      });
+    })
+    .catch(err => {
+      res.status(409).json({
+        error: 'User already exists'
+      });
+    });
 };
