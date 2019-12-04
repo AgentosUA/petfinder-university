1b4a9c7938f8b21b200b7c6710708496a7355257
= <=>
2019-12-05 01:05:29 +0200

    added checkAuth middleware to profile patch route

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const checkAuth = require('../middleware/check-auth');
 const profileController = require('../controllers/profile.js');
 
 router.get('/profile/:id', profileController.getProfile);
-router.patch('/profile/:id', profileController.patchProfile);
+router.patch('/profile/:id', checkAuth, profileController.patchProfile);
 
 module.exports = router;
