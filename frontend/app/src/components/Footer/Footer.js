3702e7a17713168271240b791bd43632961fe094
AgentosUA <agentoswork@gmail.com>
2020-02-06 21:26:39 +0200

    added footer component

--- frontend/app/src/components/Footer/Footer.js
+++ frontend/app/src/components/Footer/Footer.js
@@ -1,8 +1,39 @@
 import React, { Component } from 'react';
 
+import { NavLink } from 'react-router-dom';
+
+import Logo from '../../images/logo.png';
+import githubIcon from '../../images/icons/git-footer.png';
+import facebookIcon from '../../images/icons/facebook-footer.png';
+
 class Footer extends Component {
   render() {
-    return <h1>Footer!</h1>;
+    return (
+      <footer className="footer">
+        <div className="wrapper">
+          <section className="footer-content">
+            <div className="copyright">
+              <NavLink to="/">
+                <img src={Logo} alt="logo" />
+              </NavLink>
+              <h3>
+                <NavLink to="/">Pet Finder</NavLink>
+              </h3>
+              <br />
+              <span>© 2019 Pet Finder Project</span>
+            </div>
+            <div className="social-icons">
+              <a href="http://github.com/AgentosUA/petfinder">
+                <img src={githubIcon} alt="" />
+              </a>
+              <a href="http://facebook.com/">
+                <img src={facebookIcon} alt="" />
+              </a>
+            </div>
+          </section>
+        </div>
+      </footer>
+    );
   }
 }
 
