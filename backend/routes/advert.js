6195ad328374ff71aab59bd03f4480be6c05ca9d
AgentosUA <agentoswork@gmail.com>
2020-02-22 12:25:28 +0200

    added filter to images that will be send

--- backend/routes/advert.js
+++ backend/routes/advert.js
@@ -24,9 +24,9 @@ const fileFilter = (req, file, cb) => {
 
 const upload = multer({
   storage: storage,
-  // limits: {
-  //   fileSize: 1024 * 1024 * 5
-  // },
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
   fileFilter: fileFilter
 });
 
