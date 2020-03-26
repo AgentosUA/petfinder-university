c3a1a6a2c0ed4c8094e2b610c5eb07d677250b83
AgentosUA <agentoswork@gmail.com>
2020-03-26 17:19:00 +0200

    adding basic post req to signup page & updated dep

--- frontend/app/src/pages/SignUp/SignUp.js
+++ frontend/app/src/pages/SignUp/SignUp.js
@@ -1,24 +1,74 @@
 import React, { Component } from 'react';
-
+import axios from 'axios'
 import { Link } from 'react-router-dom';
 
 class SignUpPage extends Component {
+  state = {
+    email: '',
+    name: '',
+    password: '',
+    repassword: ''
+  }
+
+  setInputDataToState(e) {
+    const dataType = e.target.name;
+    switch (dataType) {
+      case 'email':
+        this.setState({ email: e.target.value });
+        break;
+      case 'name':
+        this.setState({ name: e.target.value });
+        break;
+      case 'password':
+        this.setState({ password: e.target.value });
+        break;
+      case 'repassword':
+        this.setState({ password: e.target.value });
+        break;
+      default:
+        break;
+    }
+  }
+
+  signUp() {
+    let data = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password
+    }
+
+    if (this.state.password !== this.state.repassword) {
+      alert("Паролі не співпадають!");
+    } else {
+      axios.post('/signup', data)
+        .then(result => {
+          alert(result);
+        })
+        .catch(err => console.log(err));
+    }
+
+
+  }
+
   render() {
+    this.signUp = this.signUp.bind(this);
+    this.setInputDataToState = this.setInputDataToState.bind(this);
+
     return (
       <main>
         <div className="wrapper">
           <section className="login">
             <h2>Реєстрація</h2>
-            <form method="POST">
-              <input type="text" placeholder="Email" required />
-              <input type="text" placeholder="Ім'я" required />
-              <input type="password" placeholder="Пароль" required />
-              <input type="password" placeholder="Повторіть пароль" required />
+            <form>
+              <input type="text" name="email" placeholder="Email" required onChange={this.setInputDataToState} />
+              <input type="text" name="name" placeholder="Ім'я" required onChange={this.setInputDataToState} />
+              <input type="password" name="password" placeholder="Пароль" required onChange={this.setInputDataToState} />
+              <input type="password" name="repassword" placeholder="Повторіть пароль" required onChange={this.setInputDataToState} />
               <div className="buttons">
                 <span type="submit">
                   <Link to="/login">Вже маєте акаунт?</Link>
                 </span>
-                <button className="sumbit" type="submit">
+                <button className="sumbit" onClick={this.signUp}>
                   Зареєструватися
                 </button>
               </div>
