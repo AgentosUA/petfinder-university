8653c92c54bf18ae2d8a5cae2855d7f6361d4b7d
AgentosUA <agentoswork@gmail.com>
2020-04-13 17:36:27 +0300

    removed checkAuth middleware for getProfile

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const profileController = require('../controllers/profile.js');
 
-router.get('/profile/:id', checkAuth, profileController.getProfile);
+router.get('/profile/:id', profileController.getProfile);
 router.post('/profile/add-pet', checkAuth, profileController.postPet);
 router.patch('/profile/:id', checkAuth, profileController.patchProfile);
 
