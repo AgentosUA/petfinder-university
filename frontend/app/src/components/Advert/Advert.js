d898906aed5fc3e5e259f1ba1d93700b8707cad2
AgentosUA <agentoswork@gmail.com>
2020-04-15 01:32:48 +0300

    added Advert component

--- frontend/app/src/components/Advert/Advert.js
+++ frontend/app/src/components/Advert/Advert.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 
 class Advert extends Component {
   render() {
+    let breed;
+    if (this.props.breed) {
+      breed = (
+        <p>
+          <span>Порода: </span>
+          {this.props.breed}
+        </p>
+      );
+    }
+    let gender;
+    switch (this.props.gender) {
+      case 'he':
+        gender = 'Він';
+        break;
+      case 'she':
+        gender = 'Вона';
+        break;
+      default:
+        gender = 'Невідомо';
+        break;
+    }
     return (
       <div className="item">
         <img src={this.props.src} alt={this.props.name} />
         <h3>{this.props.name}</h3>
-        <br />
-        {/* <span>Стать: {this.props.gender}</span> */}
-        <p>{this.props.breed ? 'Статус: ' + this.props.breed : ''}</p>
-        <br />
-        <p>{this.props.description}</p>
+
+        <p>
+          <span>Стать: </span>
+          {gender}
+        </p>
+        {breed}
+        <p>
+          <span>Опис: </span>
+          {this.props.description}
+        </p>
         <br />
       </div>
     );
