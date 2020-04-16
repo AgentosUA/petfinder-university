afaedf8110793e4f1f10385313eaf84e1da2bea9
AgentosUA <agentoswork@gmail.com>
2020-04-17 02:26:10 +0300

    moved some styles in section folder & add subtitle

--- frontend/app/src/shared/components/Section/Section.js
+++ frontend/app/src/shared/components/Section/Section.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './Section.css';
+
 const Section = (props) => {
   return (
     <section className="content-section">
       <h2 className="content-title">{props.title}</h2>
+      {props.subtitle ? <p className="content-subtitle">{props.subtitle}</p> : ''}
       <div className={props.flex ? 'center' : ''}>{props.children}</div>
     </section>
   );
