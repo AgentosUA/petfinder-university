fbaaf79ade8656ec6d70bbbfdadf3b7012930654
Agentos <agentoswork@gmail.com>
2020-05-11 20:56:18 +0300

    added modal window to SignUP page

--- frontend/app/src/pages/SignUp/SignUp.js
+++ frontend/app/src/pages/SignUp/SignUp.js
@@ -6,12 +6,15 @@ import Wrapper from '../../shared/components/Wrapper/Wrapper';
 import Section from '../../shared/components/Section/Section';
 import Button from '../../shared/components/UI/Button/Button';
 import Form from '../../shared/components/UI/Form/Form';
+import Modal from '../../shared/components/UI/Modal/Modal';
 
 const SignUp = (props) => {
   const [email, setEmail] = useState(null);
   const [name, setName] = useState(null);
   const [password, setPassword] = useState(null);
   const [repassword, setRePassword] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalMessage, setModalMessage] = useState('');
 
   const setInputDataToState = (e) => {
     const dataType = e.target.name;
@@ -33,27 +36,63 @@ const SignUp = (props) => {
     }
   };
 
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
+
   const PostSignUp = async (e) => {
     e.preventDefault();
-    if (password !== repassword) {
-      return;
+    try {
+      if (password !== repassword) {
+        setModalMessage('Паролі не співпадають!');
+        setShowModal(true);
+        return;
+      }
+
+      const data = JSON.stringify({
+        email,
+        password,
+        name,
+      });
+      const res = await axios.post('http://localhost:5000/signup', data, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (res.status === 201) {
+        setModalMessage('Профіль успішно створено!');
+        setShowModal(true);
+        return;
+      }
+
+      if (res.status === 401) {
+        setModalMessage('Такий користувач вже існує!');
+        setShowModal(true);
+        return;
+      }
+    } catch (error) {
+      // console.log(error);
+      setModalMessage('Такий користувач вже існує!');
+      setShowModal(true);
     }
-    console.log('Registred!');
+
     const data = JSON.stringify({
       name,
       email,
       password,
     });
-    const res = await axios.post('http://localhost:5000/signup', data, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    console.log(res);
   };
 
   return (
     <main className="container">
+      <Modal
+        show={showModal}
+        onCancel={closeModalHandler}
+        title={modalMessage}
+        subtitle="h3"
+        text="Lol"
+      ></Modal>
       <Wrapper>
         <Section title="Реєстрація" className="search-container" flex>
           <Form className="login-form">
@@ -93,7 +132,12 @@ const SignUp = (props) => {
             <NavLink to="/login" className="ask">
               Вже маєте акаунт?
             </NavLink>
-            <Button styles="main" type="submit" text="Зареєструватися" submit={PostSignUp} />
+            <Button
+              styles="main"
+              type="submit"
+              text="Зареєструватися"
+              submit={PostSignUp}
+            />
           </Form>
         </Section>
       </Wrapper>
