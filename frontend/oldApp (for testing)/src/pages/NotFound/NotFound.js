6c8fad6995d09cf0f3fa8251248ab2227b10ae24
Agentos <agentoswork@gmail.com>
2020-05-14 13:14:20 +0300

    removed test app

--- frontend/oldApp (for testing)/src/pages/NotFound/NotFound.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class NotFoundPage extends Component {
-  render() {
-    return <h1>NOT FOUND!</h1>;
-  }
-}
-
-export default NotFoundPage;
--- frontend/oldApp (for testing)/src/pages/Profile/Profile.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class ProfilePage extends Component {
-  render() {
-    return <h1>Profile!</h1>;
-  }
-}
-
-export default ProfilePage;
