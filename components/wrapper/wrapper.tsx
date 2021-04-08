0cb035112f9536cbd29788bfe380ab3553db1bd3
OlehStepaniuk <oleh.stepaniuk@chatbots.studio>
2021-04-08 22:12:40 +0300

    init layout

---
+++ components/wrapper/wrapper.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from 'react';
+import { Header, Footer } from '../';
+
+const Wrapper = ({ children }) => {
+  return (
+    <div className={}>
+      {children}
+    </div>
+  );
+};
+
+export { Wrapper };
