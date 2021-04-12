022d31d9e1ad41379b390c7a36ea524fbc27e636
OlehStepaniuk <oleh.stepaniuk@chatbots.studio>
2021-04-12 23:18:32 +0300

    add select & date

---
+++ modules/home/main-search/data.ts
@@ -0,0 +1,7 @@
+const animalType = [
+  { value: 'all', label: 'Тип (усі)' },
+  { value: 'he', label: 'Він' },
+  { value: 'she', label: 'Вона' }
+];
+
+export { animalType }
--- modules/home/main-search/main-search.tsx
+++ modules/home/main-search/main-search.tsx
@@ -1,12 +1,50 @@
 import styles from './main-search.module.scss';
+import Select from 'react-select'
+import DatePicker, { registerLocale } from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import uk from 'date-fns/locale/uk';
+import { animalType } from './data';
+import { useState } from 'react';
+
+registerLocale('uk', uk)
 
 const MainSearch = () => {
+  const [startDate, setStartDate] = useState(new Date());
+
+  const inputStyles = {
+    input: (provided) => {
+      return {
+        ...provided,
+        width: '105px',
+        height: '30px',
+        border: 'none'
+      }
+    },
+    controls: (provided) => {
+      return {
+        ...provided,
+        borderRadiud: 0
+      }
+    }
+  }
 
   return (
     <div className={styles.mainSearch}>
       <div className={styles.titleWrapper}>
         <h2 className={styles.title}>Pet finder</h2>
         <p className={styles.subtitle}>Пошук ваших улюбленців</p>
+      </div>
+      <div className={styles.filters}>
+        <Select options={animalType} styles={inputStyles} placeholder='Тип' />
+        <Select options={animalType} styles={inputStyles} placeholder='Стать' />
+        <Select options={animalType} styles={inputStyles} placeholder='Статус' />
+        <Select options={animalType} styles={inputStyles} placeholder='Місто' />
+        <DatePicker
+          locale="uk"
+          selected={startDate}
+          onChange={date => setStartDate(date)}
+        />
+        <button className={styles.button}>Пошук</button>
       </div>
     </div>
   )
