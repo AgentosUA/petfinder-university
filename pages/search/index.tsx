8b4f793f742602ef0ed93f72f366dcd2b57570ff
AgentosUA <oleh.stepaniuk@chatbots.studio>
2021-05-10 00:04:10 +0300

    first fetch from API

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,8 +1,41 @@
 import Head from 'next/head';
+import { useRouter } from "next/router";
 import { Container, Layout } from '../../components';
 import { MainSearch } from '@home';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+  const router = useRouter();
+  
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+
+
+  const fetchPosts = async () => {
+    try {
+      const { data } = await axios({
+        url: 'http://localhost:3000/posts',
+        method: 'POST',
+        data: {
+          page: 1,
+          limit: 99999
+        }
+      });
+
+      setPosts(data);
+      // setIsLoading(false);
+      console.log(router.query);
+    } catch (error) {
+      setIsLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchPosts();
+  }, [])
+
   return (
     <Layout>
       <Head>
@@ -10,8 +43,16 @@ export default function Home() {
       </Head>
       <MainSearch />
       <Container>
-      
+        {isLoading ? <img src='loading_light.gif' alt='Loading logo gif' /> : (
+
+          posts.map((item) => {
+            return <li>{item.name}</li>
+          })
+
+        )}
+
       </Container>
     </Layout>
   )
 }
+
