d3ae41319b2ae17376c707f08ef90481fb038b8c
AgentosUA <agentoswork@gmail.com>
2022-03-19 23:52:58 +0200

    update pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,7 @@ const { PORT } = require('./config/local.json');
 
 module.exports = {
   apps: [{
-    name: "petfinder-app",
+    name: "app-dev",
     watch: true,
     script: './node_modules/next/bin/next',
     node_args: `start -p ${PORT || 4000}`
