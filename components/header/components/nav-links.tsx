c678b4a58deb54e949d6e59f415603a7ac236055
AgentosUA <agentoswork@gmail.com>
2022-03-20 21:12:09 +0200

    test

--- components/header/components/nav-links.tsx
+++ components/header/components/nav-links.tsx
@@ -7,7 +7,7 @@ const NavLinks = () => (
     <Link href='/'>Головна</Link>
     <Link href='/search?type=all&gender=all&status=escaped&city=all&date=all'>Зниклі</Link>
     <Link href='/search?type=all&gender=all&status=founded&city=all&date=all'>Знайдені</Link>
-    <Link href='/about'>Притулки</Link>
+    <Link href='/shelter'>Притулки та добрі руки</Link>
     <Link href='/about'>Про проект</Link>
   </Fragment>
 );
